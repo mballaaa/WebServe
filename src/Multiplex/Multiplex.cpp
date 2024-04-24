944856e80773cf65849ac94651e9ad38af4ec92f
Milimas <4929474+Milimas@users.noreply.github.com>
2024-04-24 19:30:40 +0100

    cleanup

--- src/Multiplex/Multiplex.cpp
+++ src/Multiplex/Multiplex.cpp
@@ -18,7 +18,6 @@ void Multiplex::setup(const servers_t &servers)
     }
 
     servers_t::const_iterator servIt = servers.begin();
-    // each server should have a socket
     epollFD = SocketManager::createEpoll();
     while (servIt != servers.end())
     {
@@ -51,13 +50,9 @@ void Multiplex::start(void)
     eventName[EPOLLERR] = "EPOLLERR";
     eventName[EPOLLHUP] = "EPOLLHUP";
 
-    /* The event loop */
-    Http_req reqqq;
-
     while (1)
     {
-        int eventCount; // Number of events epoll_wait returned
-        /// i add this to get some data from config file like clientMaxSize
+        int eventCount;
 
         eventCount = epoll_wait(epollFD, events, SOMAXCONN, -1); // Waiting for new event to occur
         std::cerr << eventCount << " events ready" << std::endl;
@@ -78,19 +73,14 @@ void Multiplex::start(void)
 
             if ((events[i].events & EPOLLERR))
             {
-                // /* An error has occured on this fd, or the socket is not
-                //     ready for reading (why were we notified then?) */
-                // // fprintf (stderr, "epoll error\n");
                 close(events[i].data.fd);
                 perror("EPOLLERR");
                  close (events[i].data.fd);
-                //     // requests.erase(events[i].data.fd) ;
+                requests.erase(events[i].data.fd) ;
                 continue;
             }
             else if (listeners.find(events[i].data.fd) != listeners.end()) // Check if socket belong to a server
             {
-                /* We have a notification on the listening socket, which
-                    means one or more incoming connections. */
                 struct sockaddr in_addr;
                 socklen_t in_len;
                 int infd;
@@ -124,106 +114,31 @@ void Multiplex::start(void)
                     //         "(host=%s, port=%s)\n", infd, hbuf, sbuf);
                 }
 
-                /**
-                 * Make the incoming socket non-blocking and add it to the list of fds to monitor.
-                 */
                 SocketManager::makeSocketNonBlocking(infd);
                 SocketManager::epollCtlSocket(infd, EPOLL_CTL_ADD);
-                // mballa: create and add request object after accepting new client
-                // Http_req my_obg;
                 requests.insert(std::make_pair(infd, Http_req(listeners[events[i].data.fd])));
                 continue;
             }
             else if (events[i].events & EPOLLIN )  // check if we have EPOLLIN (connection socket ready to read)
             {
-                /**
-                 * We have a notification on the connection socket meaning there is more data to be read
-                 */
-
-                ssize_t bytesReceived;  // number of bytes read returned
-                char buf[R_SIZE] = {0}; // read buffer
+                ssize_t bytesReceived;
+                char buf[R_SIZE] = {0};
 
                 bytesReceived = read(events[i].data.fd, buf,  R_SIZE - 1);
-                if (bytesReceived == -1)
+                if (bytesReceived == -1 || bytesReceived == 0)
                 {
-                       std :: cout << "Jeeeeeeeerrrrrr\n";
-                    perror("read");
-                    // printf ("Closed connection on descriptor %d\n",
-                    // events[i].data.fd);
-
-                    /* Closing the descriptor will make epoll remove it
-                        from the set of descriptors which are monitored. */
                     close(events[i].data.fd);
-                    // mballa:remove request from map after closing connection
                     requests.erase(events[i].data.fd);
                     continue;
-                }
-                else if (bytesReceived == 0)
-                {
-                    std :: cout << "Jeeeeeeee\n";
-                    /* End of file. The remote has closed the
-                        connection. */
-                    // printf ("Closed connection on descriptor %d by client\n",
-                    // events[i].data.fd);
-
-                    /* Closing the descriptor will make epoll remove it
-                        from the set of descriptors which are monitored. */
-                    close(events[i].data.fd);
-                    // mballa: remove request from map after closing connection
-                    requests.erase(events[i].data.fd);
-                    continue;
-                }
-
-                /* Write the buffer to standard output */
-                std::cerr << FOREGRN;
-                std::cerr << "============== Request ==============" << std::endl;
-                std::cerr << "==============+++++++++==============" << std::endl;
-                  std::ofstream outputFile("buffer.txt", std::ios_base::app);
-
-                if (outputFile.is_open())
-                {
-                    // Output body to the file
-                    outputFile << buf;
-
-                    // Close the file
-                    outputFile.close();
                 }
                
-                std::string toSTing(buf,bytesReceived); // Convert received data to string using the total bytes received
-                 //Http_req &currRequest = requests.find(events[i].data.fd)->second;
-               // std::cout << "fddddd " << requests[events[i].data.fd].is_finsh << std::endl;
+                std::string toSTing(buf,bytesReceived);
+
                 requests[events[i].data.fd].parse_re(toSTing, bytesReceived);
-                 //currRequest.parse_re(toSTing, bytesReceived); // Pass totalBytesReceived instead of bytesReceived
-                 //reqqq = currRequest;
-
-                // //std :: cout << "HEY\n";
-
-                std::cerr << "==============+++++++++==============" << std::endl;
-                std::cerr << "==============+++++++++==============" << std::endl;
-
-                
-                std::cerr << RESETTEXT;
-                if (s == -1)
-                {
-                    perror("write");
-                    throw std::runtime_error("Could not write in ");
-                }
-
-                /**
-                 * Set connection socket to EPOLLOUT to write reponse in the next iteration
-                 * don't forget that if you didnt set the connection to EPOLLOUT the program
-                 * wont send your response and keep waiting for EPOLLIN
-                 */
-                SocketManager::epollCtlSocket(events[i].data.fd, EPOLL_CTL_MOD, EPOLLOUT);
             }
-            else if (events[i].events & EPOLLOUT && requests[events[i].data.fd].getFlag() == true) // check if we have EPOLLOUT (connection socket ready to write)
+            else if (events[i].events & EPOLLOUT && requests[events[i].data.fd].getFlag() == true)
             {
-        
-                
-                SocketManager::epollCtlSocket(events[i].data.fd, EPOLL_CTL_MOD, EPOLLIN) ;
-             
                 Response resp(requests[events[i].data.fd]);
-              //  std :: cout << "Jeeee\n" ;
                 if(resp.getResponse().empty())
                 {
                     std :: cout << "IS empty\n";
@@ -232,13 +147,10 @@ void Multiplex::start(void)
                    //  std :: cout << "IS not empty\n";
                 }
                 s = write (events[i].data.fd, resp.getResponse().c_str(), resp.getResponse().size());
-                
-                
-                   std::cout << "============== Response ==============" << std::endl ;
-               
-
+                if (s == -1)
+                    perror("write") ;
+                std::cout << "============== Response ==============" << std::endl ;
             }
         }
     }
-    // close (sfd);
 }
